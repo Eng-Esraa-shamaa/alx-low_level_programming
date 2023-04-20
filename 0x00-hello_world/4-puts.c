9cef685701479ea32c13ebffa4e19ad98a2400f8
Eng-Esraa-shamaa <esra2asmaibrahim@gmail.com>
2023-04-20 09:04:27 -0700

    4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main - the main function which returns zero
+ * puts function- used to print statement
+ * return: should return zero
+  */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+		return (0);
+}
