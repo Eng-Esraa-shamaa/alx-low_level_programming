13c11cace798ff40b816865bccc5637095d252f7
Eng-Esraa-shamaa <esra2asmaibrahim@gmail.com>
2023-04-20 10:48:06 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 /**
- *main - function return zero 
- *Description : function used to print out statement using printf function
- *Return : always will retuen zero
- */
+*main - function return zero
+*printf - function used to print statements
+*Description: function used to print out statement using printf function
+*Return: always will retuen zero
+*/
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
