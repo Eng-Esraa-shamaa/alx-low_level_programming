c3ded60b0038044907fcd774db9f0e05ac16fc3a
Eng-Esraa-shamaa <esra2asmaibrahim@gmail.com>
2023-04-21 14:35:09 -0700

    task 5. Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ *main - comment
+ *Discription: Numbers
+ *Return: return zero
+ */
+int main(void)
+{
+int n = 0;
+
+while (n <= 9)
+{
+printf("%d", n);
+n++;
+}
+printf("\n");
+return (0);
+}
