2b4cdd152c5d62b0b59228e84b888f40170416f3
Eng-Esraa-shamaa <esra2asmaibrahim@gmail.com>
2023-04-21 15:03:14 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ *main - comment
+ *Discription: Numbers
+ *Return: return zero
+ */
+int main(void)
+{
+int n = '0';
+
+while (n <= '9')
+{
+putchar(n);
+n++;
+}
+putchar('\n');
+return (0);
+}
+
