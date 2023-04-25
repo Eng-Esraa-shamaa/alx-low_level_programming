21fbe791a95d7241725c2c6a85f2b9ecb7132b3b
Eng-Esraa-shamaa <esra2asmaibrahim@gmail.com>
2023-04-25 16:58:32 -0700

    task 14

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -3,16 +3,16 @@
  *main - main function of the program
  *Return: zero
  */
-int main()
+int main(void)
 {
 int i;
-int x = 1, y= 2;
+int x = 1, y = 2;
 
 int next = x + y;
-printf("%d, %d, ", x, y);
+printf("%ld, %ld, ", x, y);
 for (i = 3; i <= 50; ++i)
 {
-printf("%d, ", next);
+printf("%ld, ", next);
 x = y;
 y = next;
 next = x + y;
