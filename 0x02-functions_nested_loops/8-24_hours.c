fdbc92628a10ad8e376678a9304adf3d2fa8675c
Eng-Esraa-shamaa <esra2asmaibrahim@gmail.com>
2023-04-25 10:27:35 -0700

    song duration

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,19 +10,19 @@ int x, y, z, m;
 
 for (x = 0; x <= 2; x++)
 {
-for ( y = 0; y <= 9; y++)
+for (y = 0; y <= 9; y++)
 {
-if (( x <= 1 && y <= 9) || (x <= 2 && y <= 3))
+if ((x <= 1 && y <= 9) || (x <= 2 && y <= 3))
 {
-for ( z = 0 ; z <= 5; z++)
+for (z = 0 ; z <= 5; z++)
 {
-for ( m = 0; m <= 9; m++)
+for (m = 0; m <= 9; m++)
 {
-_putchar(x+'0');
-_putchar(y+'0');
+_putchar(x + '0');
+_putchar(y + '0');
 _putchar(58);
-_putchar(z+'0');
-_putchar(m+'0');
+_putchar(z + '0');
+_putchar(m + '0');
 _putchar('\n');
 }
 }
