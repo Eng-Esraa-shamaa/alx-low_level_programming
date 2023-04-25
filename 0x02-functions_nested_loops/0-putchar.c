12d7379ae8b940bce5be7db7dfd49604e5bb41c8
Eng-Esraa-shamaa <esra2asmaibrahim@gmail.com>
2023-04-25 03:39:59 -0700

    task 3 c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *main - main function of the code
+ *Description: function used to print a statement
+ *Return: program should return zero
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
