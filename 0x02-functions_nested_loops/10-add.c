5478a89b9bf844acfba9c0f5610ec5075d5df22d
Eng-Esraa-shamaa <esra2asmaibrahim@gmail.com>
2023-04-25 12:20:48 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ *add - the main function of the program
+ *@x: the first integer
+ *@y: the second integer
+ *Description: function to add two integers
+ *Return: the result
+ */
+int add(int x, int y)
+{
+	return (x + y);
+}
