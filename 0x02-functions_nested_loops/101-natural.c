93f64f5c07f2abd0229cb39e940d102fe9777064
Eng-Esraa-shamaa <esra2asmaibrahim@gmail.com>
2023-04-25 17:21:17 -0700

    101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+/**
+ *main - main function of program
+ *Return: zero
+ */
+int main(void)
+{
+	int i;
+	int x = 0;
+
+	while (i < 1024)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+		{
+			x = x + i;
+		}
+		i++;
+	}
+	printf("%d", x);
+	return (0);
+}
