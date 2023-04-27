d977163e0ff03ee3cb4eee1a16b9418fe3940c2a
Eng-Esraa-shamaa <esra2asmaibrahim@gmail.com>
2023-04-27 09:44:29 -0700

    ./7-diagonals

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ *print_diagonal - used to print diagonal
+ *@n: numbers used for diagonal
+ *Return: zero
+ */
+void print_diagonal(int n)
+{
+	int x, y;
+
+	if (n > 0)
+	{
+	for (x = 1; x <= n; x++)
+	{
+		for (y = 1; y <= x; y++)
+		{
+		_putchar(32);
+		}
+		_putchar(92);
+		_putchar('\n');
+	}
+	}
+	else
+	{
+	_putchar('\n');
+	}
+}
