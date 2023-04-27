8c41477fdc5cb86f2893438d865a5aead0ab8b88
Eng-Esraa-shamaa <esra2asmaibrahim@gmail.com>
2023-04-27 07:51:23 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ *mul - function to multiply
+ *@a: digit to be multiplied
+ *@b: digit to be multiplied
+ *Return: the result
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
