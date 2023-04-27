3c37ae0e5471fe6033da652cdb1821f6c224dfb4
Eng-Esraa-shamaa <esra2asmaibrahim@gmail.com>
2023-04-27 12:32:34 -0700

    ./10-triangles

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ *print_triangle - function print triangle
+ *@size: size of the triangle
+ *Return: zero
+ */
+void print_triangle(int size)
+{
+	int i, j, x;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+		for(j = size - i; j > 1; j--)
+		{
+			_putchar(32);
+		}
+		for (x = 0; x <= i; x++)
+		{
+			_putchar(35);
+		}
+		_putchar('\n');
+		}
+	}
+}
