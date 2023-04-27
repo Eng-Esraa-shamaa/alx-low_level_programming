8d270b9c6a713e2efd5d995717e02de904352364
Eng-Esraa-shamaa <esra2asmaibrahim@gmail.com>
2023-04-27 10:26:39 -0700

    square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -8,15 +8,15 @@ void print_square(int size)
 {
 	int x, y;
 
-	if(size > 0)
+	if (size > 0)
 	{
 	for (x = 1; x <= size; x++)
 	{
-		for (y = 1; y <= size; y++)
-		{
+	for (y = 1; y <= size; y++)
+	{
 	_putchar(35);
 		}
-		_putchar('\n');
+	_putchar('\n');
 	}
 	}
 	else
