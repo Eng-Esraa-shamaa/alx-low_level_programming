20f9d97bd2c83e261ff16660cda08393b5a282ec
Eng-Esraa-shamaa <esra2asmaibrahim@gmail.com>
2023-04-27 08:22:49 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *print_numbers - function used to print numbers
+ *Description: prints numbers from zero to nine
+ *Return: zero
+ */
+void print_numbers(void)
+{
+	char i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		_putchar(i);
+	}
+	_putchat('\n');
+}
