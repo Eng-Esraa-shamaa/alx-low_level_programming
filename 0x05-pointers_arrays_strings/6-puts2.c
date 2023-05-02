4db40763714437fe486687a64a75e1f72a664444
Eng-Esraa-shamaa <esra2asmaibrahim@gmail.com>
2023-05-02 06:57:31 -0700

    puts

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *puts2 - function prints char of a string
+ *@str: the string to be printed
+ *Return: zero
+ */
+void puts2(char *str)
+{
+	int j = 0, i;
+
+	while (str[j] != '\0')
+	{
+		j++;
+	}
+	for (i = 0; i <= j; i = i + 2)
+	{
+	_putchar(str[i]);
+	}
+	_putchar('\n');
+}
