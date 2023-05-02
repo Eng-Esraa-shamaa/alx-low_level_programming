10a89124a790c2ede81fb275d27e3a5888376faa
Eng-Esraa-shamaa <esra2asmaibrahim@gmail.com>
2023-05-02 08:12:55 -0700

    task 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *_strcpy - function to copy the string
+ *@dest: the first string
+ *@src: the string to be copied into dest
+ *Return: the pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int x;
+
+	for (x = 0; src[x] != '\0'; x++)
+	{
+		dest[x] = src[x];
+	}
+	dest[x + 1] = '\0';
+return (dest);
+}
