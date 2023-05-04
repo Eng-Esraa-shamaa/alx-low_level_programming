aa63f0e9c0e880e8ba8e9e2616e0a4d820c3014f
Eng-Esraa-shamaa <esra2asmaibrahim@gmail.com>
2023-05-04 11:01:13 -0700

    7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ *leet - encodes the string
+ *@s: string to be encoded
+ *Return: the encoded string
+ */
+char *leet(char *s)
+{
+	int i;
+	int j;
+	char str1[] = "aAeEoOtTlL";
+	char str2[] = "4433007711";
+
+for (i = 0; s[i] != '\0'; i++)
+{
+	for (j = 0; j < 10; j++)
+	{
+		if (s[i] == str1[j])
+		{
+			s[i] = str2[j];
+		}
+	}
+}
+return (s);
+}
