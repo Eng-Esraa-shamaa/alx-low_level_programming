45c2ef2186bfb7c5548797e1b571049d7698259b
Eng-Esraa-shamaa <esra2asmaibrahim@gmail.com>
2023-05-04 05:06:35 -0700

    compare

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,19 +7,15 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0;
+	int j = 0;
 
-	while (s1[i] != '\0' && s2[i] != '\0')
+	while (s1[j] != '\0' && s2[j] != '\0')
 	{
-		if (s1[i] == s2[i])
+		if (s1[j] != s2[j])
 		{
-			return (0);
+			return (s1[j] - s2[j]);
 		}
-		else if (s1[i] != s2[i])
-		{
-			return (s1[i] - s2[i]);
-		}
-		i++;
+		j++;
 	}
 	return (0);
 }
