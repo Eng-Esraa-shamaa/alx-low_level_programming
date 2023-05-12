1fe8aafabe506954ba653265229b22b0d627c112
Eng-Esraa-shamaa <esra2asmaibrahim@gmail.com>
2023-05-12 02:44:33 -0700

    asma

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -11,16 +11,21 @@
 int main(int argc, char *argv[])
 {
 	int i;
-	unsigned int sum = 0;
+	unsigned int j, sum = 0;
+	char *s;
 
 if (argc > 1)
 {
 	for (i = 1; i < argc; i++)
 	{
-		if (*argv[i] < 48 || *argv[i] > 57)
+	s = argv[i];
+		for (j = 0; j < strlen(s); j++)
+		{
+		if (s[j] < 48 || s[j] > 57)
 		{
 			printf("Error\n");
 			return (1);
+		}
 		}
 			sum = sum + atoi(argv[i]);
 	}
