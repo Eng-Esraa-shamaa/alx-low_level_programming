0db4b59309615eb2f826201a471b41bcd2b86a4e
Eng-Esraa-shamaa <esra2asmaibrahim@gmail.com>
2023-05-12 00:45:21 -0700

    argc and argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include<stdio.h>
+/**
+ *main - the main function to the program
+ *@argc: the number of arguments
+ *@argv: the vector to arguments
+ *Return: the name of the program
+ */
+int __attribute__ ((unused)) main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
