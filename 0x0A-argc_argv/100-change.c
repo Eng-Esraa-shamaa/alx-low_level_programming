76a149476e3a4dc4651c348af8dd0eb2fe6a45a8
Eng-Esraa-shamaa <esra2asmaibrahim@gmail.com>
2023-05-12 13:04:29 -0700

    argc and argv advanced

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,51 @@
+#include "main.h"
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ *main - the main function of the program
+ *@argc: the number of arguments in array
+ *@argv: the array contains arguments
+ *Return: zero in success case
+ */
+int main(int argc, char *argv[])
+{
+	int money, change = 0;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	money = atoi(argv[1]);
+	while (money > 0)
+	{
+		change++;
+		if (money - 25 >= 0)
+		{
+			money = money - 25;
+			continue;
+		}
+		if (money - 10 >= 0)
+		{
+			money = money - 10;
+			continue;
+		}
+		if (money - 5 >= 0)
+		{
+			money = money -5;
+			money -= 5;
+		}
+		if (money - 2 >= 0)
+		{
+			money = money -2;
+			continue;
+		}
+		money--;
+	}
+	printf("%d\n", change);
+	if (money < 0)
+        {
+                printf("0\n");
+        }
+	return (0);
+}
