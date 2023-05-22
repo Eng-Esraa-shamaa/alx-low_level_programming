1d8d617c4548fc71725a90fa2b007e6a6c2a3cce
Eng-Esraa-shamaa <esra2asmaibrahim@gmail.com>
2023-05-22 01:41:15 -0700

    dog only thing loves you

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include"dog.h"
+/**
+ *init_dog - initialize a variable of type struct
+ *@d: name of struct
+ *@name: name of dog
+ *@age: age of dog
+ *@owner: the owner of dog
+ *Return: zero when success
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+	{
+	d = malloc(sizeof(struct dog));
+	}
+	d->name = name;
+	d->age = age;
+	d->owner = owner;	
+}
