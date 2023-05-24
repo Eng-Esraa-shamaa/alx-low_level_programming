f34322d0712061d36dece3248c7f10ee52c3a941
Eng-Esraa-shamaa <esra2asmaibrahim@gmail.com>
2023-05-24 03:45:58 -0700

    calculator

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -3,16 +3,13 @@
 #include<stdlib.h>
 /**
  *main - main function of program
- *@agrc: number of arguments
+ *@argc: number of arguments
  *@argv: array of arguments
- *@a: first number
- *@b: second number
- *@s: the symbol
  *Return: zero when success
  */
 int main(int argc, char *argv[])
 {
-	int a , b;
+	int a, b;
 	char *s;
 
 	if (argc != 4)
@@ -26,7 +23,7 @@ int main(int argc, char *argv[])
 	if (get_op_func(s) == NULL || s[1] != '\0')
 	{
 		printf("Error\n");
-		return(99);
+		return (99);
 	}
 	printf("%d\n", get_op_func(s)(a, b));
 	return (0);
