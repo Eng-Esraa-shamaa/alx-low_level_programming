dd5de00233a81b7a4f1ca77e7977c357cffead62
Eng-Esraa-shamaa <esra2asmaibrahim@gmail.com>
2023-06-01 01:04:35 -0700

    0. Print list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+/**
+ *print_list - function that prints all the elements odf list
+ *@h: the pointer to list
+ *Return: all elements of the list
+ */
+size_t print_list(const list_t *h)
+{
+	size_t list;
+
+	list = 0;
+	while (h)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%d] (%s)\n", h->len, h->str);
+		}
+		list++;
+		h = h->next;
+	}
+	return (list);
+}
