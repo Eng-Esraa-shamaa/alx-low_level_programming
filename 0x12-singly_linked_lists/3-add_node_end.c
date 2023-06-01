c2f2369a71574f6bef3371242396940a8e1a77d5
Eng-Esraa-shamaa <esra2asmaibrahim@gmail.com>
2023-06-01 02:32:54 -0700

    fixxx

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -9,32 +9,31 @@
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new, *old;
-	int new_len;
+list_t *new, *old = *head;
+int new_len;
 
-	new = malloc(sizeof(list_t));
-	if (new == NULL)
-	{
-		return (NULL);
-	}
-	new_len = 0;
-	while (str[new_len])
-        {
-                new_len++;
-        }
-	new->str = strdup(str);
-	new->len = new_len;
-        new->next = NULL;
-	old = *head;
-	if (old == NULL)
-	{
-		old = new;
-		return (new);
-	}
-	while (old->next != NULL)
-	{
-		old = old->next;
-	}
-	old->next = new;
-	return (new);
+new = malloc(sizeof(list_t));
+if (new == NULL)
+{
+return (NULL);
+}
+new_len = 0;
+while (str[new_len])
+{
+new_len++;
+}
+new->str = strdup(str);
+new->len = new_len;
+new->next = NULL;
+if (*head == NULL)
+{
+*head = new;
+return (new);
+}
+while (old->next != NULL)
+{
+old = old->next;
+}
+old->next = new;
+return (new);
 }
