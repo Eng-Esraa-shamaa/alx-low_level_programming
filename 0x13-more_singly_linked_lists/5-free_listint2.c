35c182bec47d0d0b46b5e6683728ec2938526bb0
Eng-Esraa-shamaa <esra2asmaibrahim@gmail.com>
2023-06-05 08:26:55 -0700

    edit

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,13 +9,17 @@
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *list;
-	
-	while (*head != NULL)
-	{
-		list = (*head)->next;
-		free(*head);
-		*head = list;
-	}
-	*head = NULL;
+listint_t *list;
+
+if (*head == NULL)
+{
+return;
+}
+while (*head != NULL)
+{
+list = (*head)->next;
+free(*head);
+*head = list;
+}
+*head = NULL;
 }
