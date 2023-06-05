79d04e2354a97db765cc0bad4a2bca5bed733e7f
Eng-Esraa-shamaa <esra2asmaibrahim@gmail.com>
2023-06-05 13:02:47 -0700

    add

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -29,7 +29,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	}
 	else
 	{
-		for (i = 0; i < index; i++)
+		for (i = 0; (i < index && new != NULL); i++)
 		{
 			old = new;
 			new = new->next;
