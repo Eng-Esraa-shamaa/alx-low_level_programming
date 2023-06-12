5e8add840497536176e2de7eed74c52100da6f76
Eng-Esraa-shamaa <esra2asmaibrahim@gmail.com>
2023-06-12 13:52:19 -0700

    elf- final

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -129,11 +129,11 @@ void print_osabi(unsigned char *e_ident)
 		printf("<unknown: %x>\n", e_ident[EI_OSABI]);
 }
 /**
- *print_abi - Prints the ABI version.
+ *print_abiv - Prints the ABI version.
  *@e_ident: pointer to an array.
  *Return: nothing
  */
-void print_abi(unsigned char *e_ident)
+void print_abiv(unsigned char *e_ident)
 {
 	printf("  ABI Version:                       %d\n", e_ident[EI_ABIVERSION]);
 }
@@ -147,9 +147,7 @@ void print_type(unsigned int e_type, unsigned char *e_ident)
 {
 	if (e_ident[EI_DATA] == ELFDATA2MSB)
 		e_type >>= 8;
-
 	printf("  Type:                              ");
-
 	if (e_type == ET_NONE)
 		printf("NONE (None)\n");
 	else if (e_type == ET_REL)
@@ -172,7 +170,6 @@ void print_type(unsigned int e_type, unsigned char *e_ident)
 void print_entry(unsigned long int e_entry, unsigned char *e_ident)
 {
 	printf("  Entry point address:               ");
-
 	if (e_ident[EI_DATA] == ELFDATA2MSB)
 	{
 		e_entry = ((e_entry << 8) & 0xFF00FF00) |
@@ -190,7 +187,7 @@ void print_entry(unsigned long int e_entry, unsigned char *e_ident)
 }
 /**
  *close_elf - used to handle closing the file
- *@elf: the elf file
+ *@elf_file: the elf file
  *Return: nothing
  */
 void close_elf(int elf_file)
@@ -240,7 +237,7 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 	print_data(header->e_ident);
 	print_version(header->e_ident);
 	print_osabi(header->e_ident);
-	print_abi(header->e_ident);
+	print_abiv(header->e_ident);
 	print_type(header->e_type, header->e_ident);
 	print_entry(header->e_entry, header->e_ident);
 	free(header);
