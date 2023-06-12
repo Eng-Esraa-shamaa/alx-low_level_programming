8d998b2d4bd45680b7a9447bcaebcd62245b802c
Eng-Esraa-shamaa <esra2asmaibrahim@gmail.com>
2023-06-12 01:56:12 -0700

    Tread lightly, she is near

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ *read_textfile - function that reads a text file and prints it to the POSIX.
+ *@filename: name of the file.
+ *@letters: the letters of file
+ *Return: zero when success
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t fp;
+	char *buff;
+	ssize_t r, w;
+
+	fp = open(filename, O_RDONLY);
+	if (filename == NULL)
+	{
+		return (0);
+	}
+	buff = malloc(sizeof(char) * letters);
+	if (buff == NULL)
+	{
+		return (0);
+	}
+	r = read(fp, buff, letters);
+	w = write(STDOUT_FILENO, buff, r);
+	free(buff);
+	close(fp);
+	return (w);
+}
