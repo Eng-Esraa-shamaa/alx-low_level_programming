3d9bfed519c9db38d4701e7b9e2350c1ecdb6b15
Eng-Esraa-shamaa <esra2asmaibrahim@gmail.com>
2023-06-12 04:06:33 -0700

    edit

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,22 +16,19 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 600);
+	if (text_content != NULL)
+	{
+		while (text_content[length] != '\0')
+		length++;
+	}
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (fd == -1)
 	{
 		return (-1);
 	}
-	if (text_content != NULL)
-	{
-		while (text_content[length] != '\0')
-		{
-			length++;
-		}
-	}
 	w = write(fd, text_content, length);
 	if (w == -1)
 	{
-		close(fd);
 		return (-1);
 	}
 	close(fd);
