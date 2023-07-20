5c9ea3ea4c485498f112290a9eea01afae99dd26
Eng-Esraa-shamaa <esra2asmaibrahim@gmail.com>
2023-07-20 09:51:50 -0700

    jj

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -14,10 +14,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	{
 		return (add_dnodeint(h, n));
 	}
-	if (head->next == NULL)
-	{
-		return (add_dnodeint_end(h, n));
-	}
 	while (idx != 1)
 	{
 		head = head->next;
@@ -26,6 +22,10 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			return (NULL);
 		}
 		idx--;
+	}
+	if (head->next == NULL)
+	{
+		return (add_dnodeint_end(h, n));
 	}
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
