88aba594a5ee1efee3d1ed736b9d7710092f65e2
Eng-Esraa-shamaa <esra2asmaibrahim@gmail.com>
2023-11-13 14:29:47 +0200

    .

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,26 @@
+#include "search_algos.h"
+/**
+ *linear_search - searches for value in an array of integers using Linear search
+ *@array: the array of integers
+ *@size: size of array
+ *@value: the value to search for
+ *Return: the wanted value
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+	if (array == NULL)
+	{
+		return -1;
+	}
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+		{
+			return (i);
+		}
+	}
+	return (-1);
+}
